8f115275c261d017399e80e5d2cd8ab0984252ac
Will Runyon <william.runyon@gmail.com>
2024-08-22 16:56:43 -0600

    Add scrollrestoration

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,10 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 
 import ButtonAppBar from "../components/AppBar";
 
 function RootLayout() {
   return (
     <>
+      <ScrollRestoration />
       <ButtonAppBar />
       <Outlet />
     </>
